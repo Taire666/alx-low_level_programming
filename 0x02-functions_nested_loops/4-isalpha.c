7a7810594cc33311867b247e8bc45f6a8fbb9fd9
root <tairekite@gmail.com>
2023-03-22 07:43:27 -0700

    101-natural.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,6 +5,7 @@
      * Return: 1 if c is a letter, 0 otherwise
       */
 int _isalpha(int c)
+
 {
-	        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
